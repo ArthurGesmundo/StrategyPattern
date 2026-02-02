008b71ac1d6a281f1e68ee80a58d51929943d63f
Arthur Gesmundo <niahstic@gmail.com>
2026-02-03 07:17:52 +0800

    Change defense action from shield to dodging

--- Archer.java
+++ Archer.java
@@ -3,6 +3,6 @@ public class Archer implements Strategy{
         System.out.println("Archer shoots an arrow!");
     }
     public void defend(){
-        System.out.println("Using a shield to defend!");
+        System.out.println("Dodging to avoid attack!");
     }
 }
